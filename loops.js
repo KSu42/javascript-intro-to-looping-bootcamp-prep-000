206a7333b837c6a3f557bf391381b66c58ab992d
Kevin Sudor <ksudor42@gmail.com>
2018-04-11 15:25:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 
 function doWhileLoop(array) {
   do {
-    array = array.slice(1);
+    array = array.pop();
   } while (array.length > 0 && maybeTrue());
   return array;
 }
